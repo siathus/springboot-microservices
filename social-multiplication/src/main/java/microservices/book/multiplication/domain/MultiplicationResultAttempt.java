a097324efb76a43a6c535afb8f411450c54243ad
Seong-Gook Lim <sklim0932@gmail.com>
2019-04-29 12:48:52 +0900

    Add files via upload

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/domain/MultiplicationResultAttempt.java
@@ -0,0 +1,24 @@
+package microservices.book.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class MultiplicationResultAttempt {
+
+    private final User user;
+    private final Multiplication multiplication;
+    private final int resultAttempt;
+
+    // JSON (역)직렬화를 위한 빈 생성자
+    MultiplicationResultAttempt() {
+        user = null;
+        multiplication = null;
+        resultAttempt = -1;
+    }
+}
---
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -0,0 +1,68 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import microservices.book.multiplication.domain.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+public class MultiplicationServiceImplTest {
+
+    private MultiplicationServiceImpl multiplicationServiceImpl;
+
+    @Mock
+    private RandomGeneratorService randomGeneratorService;
+
+    @Before
+    public void setUp() {
+        // 목 객체를 초기화한다.
+        MockitoAnnotations.initMocks(this);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
+    @Test
+    public void createRandomMultiplicationTest() {
+        // given(목 객체가 처음에 50, 나중에 30을 반환하도록 설정)
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+        // when
+        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+
+        // assert
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+    }
+
+    @Test
+    public void checkCorrectAttemptTest() {
+        // given
+        Multiplication multiplication = new Multiplication(50, 60);
+        User user = new User("John_doe");
+        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000);
+
+        // when
+        boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+
+        // assert
+        assertThat(attemptResult).isTrue();
+    }
+
+    @Test
+    public void checkWrongAttemptTest() {
+        // given
+        Multiplication multiplication = new Multiplication(50, 60);
+        User user = new User("John_doe");
+        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3010);
+
+        // when
+        boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+
+        // assert
+        assertThat(attemptResult).isFalse();
+    }
+}
